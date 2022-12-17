87d0838389140541a5b6be1a9159175d4242f7dd
richou <richou672005@protonmail.com>
2022-12-17 15:39:31 +0100

    first update

---
+++ install.sh
@@ -0,0 +1,9 @@
+chown root start_rgb.service
+chown root tuxedo_keyboard.py
+chown root core.py
+cp start_rgb.service /etc/systemd/system
+mkdir /etc/rgb_http_server/
+cp core.py /etc/rgb_http_server/
+cp tuxedo_keyboard.py /etc/rgb_http_server/
+systemctl enable start_rgb.service
+systemctl start start_rgb.service
